b9ba93c7715eb34a8a1483087589f63336b0c2e7
Chris Mann <christophermmann@gmail.com>
2013-04-19 11:25:36 +0100

    Changed to ssl

--- example.rb
+++ example.rb
@@ -1,20 +1,20 @@
 require 'rest_client'
 require 'json'
 
-response = RestClient.post('http://api.project-ginsberg.com:2404/users/login', 
+response = RestClient.post('https://api.project-ginsberg.com/users/login', 
                                {"username" => "allen@project-ginsberg.com",
                                 "password" => "howl"}.to_json,
                                 :content_type =>:json, :accept =>:json)
 
 parsedJson = JSON.parse(response)
 
-response = RestClient.get('http://api.project-ginsberg.com:2404/users/' + parsedJson["uid"], 
+response = RestClient.get('https://api.project-ginsberg.com/users/' + parsedJson["uid"], 
                             {:cookies=> {"sid" => parsedJson["id"]}})
 
-response = RestClient.get('http://api.project-ginsberg.com:2404/obj-fitness?uid='+parsedJson["uid"],
+response = RestClient.get('https://api.project-ginsberg.com/obj-fitness?uid='+parsedJson["uid"],
                             {:cookies=> {"sid" => parsedJson["id"]}})
 
-response = RestClient.post('http://api.project-ginsberg.com:2404/obj-fitness',
+response = RestClient.post('https://api.project-ginsberg.com/obj-fitness',
                             {"uid"=>parsedJson["uid"],
                              "startTimestamp"=>Time.now.to_i,
                              "endTimestamp"=>Time.now.to_i+60,
